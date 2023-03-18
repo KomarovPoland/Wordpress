51f939b6bd8e9712edfe0b25608190d5ea30f44b
KomarovPoland <128140302+KomarovPoland@users.noreply.github.com>
2023-03-18 07:08:29 +0100

    Delete paragon directory

--- paragon/index.php
+++
@@ -1,5 +0,0 @@
-<div class="col-lg-9 mb-3 page-content">
-	
-	index
-
-</div>
--- paragon/sidebar.php
+++
@@ -1,9 +0,0 @@
-<aside class="sidebar col-lg-3 mb-3 d-none d-lg-block">
-
-	<div>
-		<?php if ( is_active_sidebar( 'sidebar-1' ) ) : ?>
-			<?php dynamic_sidebar( 'sidebar-1' ); ?>
-		<?php endif; ?>
-	</div>
-
-</aside>
